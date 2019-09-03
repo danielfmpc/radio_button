4abbaa4259ed45b71cef174e4e9f8dca09055f4e
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-09-03 10:46:38 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:checkbox/EntradaCheckbox.dart';
+import 'package:flutter/material.dart';
+// import 'CampoTexto.dart';
+
+void main(){
+	runApp(
+		MaterialApp(
+			// home: CampoTexto(),
+			home: EntradaCheckbox(),
+			debugShowCheckedModeBanner: false,
+		),
+	);
+}
